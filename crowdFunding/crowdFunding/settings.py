1c2e3ee1f18b318e773a116f40c35a986dc671f4
Ahmed-AbdElRhman <abdalrhmanahmed469@gmail.com>
2023-02-12 07:17:36 +0200

    Create and initiate Project APP

--- crowdFunding/crowdFunding/settings.py
+++ crowdFunding/crowdFunding/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'indexapp.apps.IndexappConfig',
+    'projectsapp.apps.ProjectsappConfig',
 
 ]
 
--- crowdFunding/crowdFunding/urls.py
+++ crowdFunding/crowdFunding/urls.py
@@ -19,4 +19,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('indexapp.urls')),
+    path('projects', include('projectsapp.urls')),
 ]
