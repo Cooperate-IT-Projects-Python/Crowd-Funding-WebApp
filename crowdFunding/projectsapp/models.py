8860e208bb407324d9508f3003f448e9f80d0be1
ayahassanali96 <111661698+ayahassanali96@users.noreply.github.com>
2023-02-13 08:47:38 +0200

    ADD Project Creation,UserApp,Initail Migration

--- crowdFunding/projectsapp/models.py
+++ crowdFunding/projectsapp/models.py
@@ -1,3 +1,35 @@
 from django.db import models
+from datetime import datetime
+
 
 # Create your models here.
+class Category(models.Model):
+    name = models.CharField(max_length=10, unique=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Project(models.Model):
+    title = models.CharField(max_length=10, unique=True)
+    details = models.CharField(max_length=50)
+    rate = models.IntegerField(default=0)
+    total_target = models.IntegerField(null=False, blank=False)
+    current_donation = models.IntegerField(default=0)
+    start_campaign = models.DateField(default=datetime.now, blank=True)
+    end_campaign = models.DateField(null=False, blank=False)
+    created_at = models.DateField(default=datetime.now, blank=True)
+    selected_at_by_admin = models.BooleanField(default=False)
+    category_id = models.ForeignKey(to="Category", on_delete=models.CASCADE)
+    owner_id = models.ForeignKey(to="users.CustomUser", on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.title
+
+
+class Projects_pictures(models.Model):
+    image = models.ImageField(upload_to='media/Projects/%y/%m/%d', null=False, blank=False)
+    ProjectId = models.ForeignKey(to="Project", on_delete=models.PROTECT)
+
+    def __str__(self):
+        return self.ProjectId.title
--- crowdFunding/projectsapp/views.py
+++ crowdFunding/projectsapp/views.py
@@ -1,6 +1,44 @@
 from django.shortcuts import render
+from django.http import HttpResponseRedirect, HttpResponse
+from .models import Category, Project, Projects_pictures
+from users.models import CustomUser
 
 
 # Create your views here.
-def initFunction(r):
-    pass
+def projectForm(r):
+    if r.method == 'POST':
+        # userid = r.session['userId']
+        userid = 3
+        if userid:
+            try:
+                # Create PROJECT
+                projobject = Project()
+                projobject.title = r.POST['title']
+                projobject.details = r.POST['details']
+                projobject.total_target = r.POST['target']
+                projobject.start_campaign = r.POST['start']
+                projobject.end_campaign = r.POST['end']
+                category = Category.objects.get(name=r.POST['category'])
+                projobject.category_id = category
+                user = CustomUser.objects.get(id=userid)
+                projobject.owner_id = user  # Session
+                projobject.save()
+                # Read Images
+                images = r.FILES.getlist('images')
+                # Table Projects_pictures
+                for image in images:
+                    projects_picturesobj = Projects_pictures()
+                    projects_picturesobj.image = image
+                    projects_picturesobj.ProjectId = projobject
+                    projects_picturesobj.save()
+            except Exception as e:
+                # Replace This  With Flash
+                #return HttpResponse("Enter Valid Data title should Be unique")
+                categories = Category.objects.all()
+                return render(r, 'createProject.html', {'categories': categories, 'error': e})
+        else:
+            # Replace This  With Flash or redirect to loginPage
+            return HttpResponse("You Should Login First")
+    # If Not Post
+    categories = Category.objects.all()
+    return render(r, 'createProject.html', {'categories': categories})
