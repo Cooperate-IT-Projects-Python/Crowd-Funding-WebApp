4da2430772ddaf23d9b3e98ac24d69b30c58553d
bedaba edward <bedaba15@gmail.com>
2023-02-14 22:59:00 +0200

    create user profile with full functionality

--- crowdFunding/indexapp/urls.py
+++ crowdFunding/indexapp/urls.py
@@ -4,4 +4,7 @@ from . import views
 urlpatterns = [
     path('', views.index, name='indexPage'),
     path('rate/<int:project_id>/<int:rating>/', views.rate, name='rating'),
+    path('edit_user_profile/', views.edit_user_profile, name='edit_user_profile'),
+    path('delete_user_profile/', views.delete_user_profile, name='delete_user_profile'),
+    path('user_profile/', views.user_profile, name='user_profile'),
 ]
--- crowdFunding/indexapp/views.py
+++ crowdFunding/indexapp/views.py
@@ -2,6 +2,12 @@ from django.shortcuts import render
 from projectsapp.models import Project, ProjectRating
 from users.models import CustomUser
 
+from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth import get_user_model
+from django.contrib import messages
+
+User = get_user_model()
 
 # Create your views here.
 def index(r):
@@ -26,3 +32,52 @@ def rate(r, project_id: int, rating: int):
     ProjectRating.objects.filter(ProjectId=project, owner_id=user).delete()
     ProjectRating.objects.create(ProjectId=project, owner_id=user, rating=rating)
     return index(r)
+
+
+
+@login_required
+def user_profile(request):
+    user = request.user
+    projects = Project.objects.filter(owner_id=user)
+    ratings = ProjectRating.objects.filter(owner_id=user)
+    context = {
+        'user': user,
+        'projects': projects,
+        'ratings': ratings
+    }
+    return render(request, 'user_profile.html', context)
+
+@login_required
+def edit_user_profile(request):
+    if request.method == 'POST':
+        user = request.user
+        user.first_name = request.POST['first_name']
+        user.last_name = request.POST['last_name']
+        user.mobile = request.POST['mobile']
+        user.birthday = request.POST['birthday']
+        user.country = request.POST['country']
+        user.facebook_profile = request.POST['facebook_profile']
+        user.save()
+        messages.success(request, 'Your profile has been updated successfully.')
+        return redirect('user_profile')
+    else:
+        user = request.user
+        context = {
+            'user': user
+        }
+        return render(request, 'edit_user_profile.html', context)
+
+@login_required
+def delete_user_profile(request):
+    if request.method == 'POST':
+        user = request.user
+        password = request.POST.get('password')
+        if user.check_password(password):
+            user.delete()
+            messages.success(request, 'Your account has been deleted successfully.')
+            return redirect('/')
+        else:
+            messages.error(request, 'Wrong password. Please try again.')
+            return redirect('delete_user_profile')
+    else:
+        return render(request, 'delete_user_profile.html')
