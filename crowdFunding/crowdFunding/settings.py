2a9b3daf46afa00ee3155e066b50670a12b1c406
mohamedsto7y <mohamed_alsayed@live.com>
2023-02-15 18:31:38 +0200

    proper settings

--- crowdFunding/crowdFunding/settings.py
+++ crowdFunding/crowdFunding/settings.py
@@ -84,7 +84,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME':  'crowd_founding',
         'USER': 'postgres' ,
-        'PASSWORD':  'POSTGRES_PASSWORD',
+        'PASSWORD':  POSTGRES_PASSWORD,
         'HOST':  '127.0.0.1',
         'PORT':  '5432'
     }
